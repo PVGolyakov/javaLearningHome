175e614ee7c773d24c2bbb3f7f5f01130c59fd8a
Pavel V Golyakov <pgolyakov@alfabank.ru>
2017-10-03 11:55:32 +0300

    Практика с JUnit #7 Вчерашние исправления + закрытие браузера.

--- src/main/java/ru/alfabank/pgolyakov2/LearnSelenium.java
+++ src/main/java/ru/alfabank/pgolyakov2/LearnSelenium.java
@@ -1,6 +1,7 @@
 package ru.alfabank.pgolyakov2;
 
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -9,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Driver;
 import java.util.Objects;
 
 import static java.lang.Thread.sleep;
@@ -20,44 +22,50 @@ import static org.junit.Assert.assertEquals;
  */
 public class LearnSelenium {
     LearnSelenium learnSelenium;
-    @Before
-    public static void main(String[] args) throws InterruptedException {
+    static WebDriver driver;
+    @BeforeClass
+    public static void openBrowser () {
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\Pavel\\Downloads\\selenium-drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        //заходим на сайт АБ и переходим в карты
-        driver.get("https://alfabank.ru/");
-        sleep(2000);
-        WebElement findBancCard = driver.findElement(By.xpath
-                ("//*[@id=\"home_page\"]/div[1]/div[1]/div/div/div[4]/ul[1]/li[2]/a"));
-        findBancCard.click();
-        sleep(4000);
-
-        //Выбор дебетовой карты
-        WebElement clickDebetCard = driver.findElement(By.xpath
-                ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[1]/div/div[2]/ul/li[1]/a"));
-        clickDebetCard.click();
-        sleep(4000);
-
-        //Выбор МастерКард и тип Классик
-        WebElement clickMasterCard = driver.findElement(By.xpath
-                ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[2]/div[1]/div[1]/div/form/div/div[3]/div/div/div/div[1]/label"));
-        clickMasterCard.click();
-
-        WebElement clickClassic = driver.findElement(By.xpath
-                ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[2]/div[1]/div[1]/div/form/div/div[4]/div/div/div/div[1]/label"));
-        clickClassic.click();
-        sleep(4000);
-
-        WebElement checkCard = driver.findElement(By.xpath
-                ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div[6]/div/div[3]/h3/a"));
+        driver = new ChromeDriver();
     }
-    @Test
-        public void shouldCheckCardName() {
-            String checkCard = "Карта «Перекрестк»";
-        if (Objects.equals(checkCard, "Карта «Перекрестк»")) {
-        }
-        throw new RuntimeException("Карта неверна");
+
+
+   @Test
+   public void shouldCheckCardName() throws InterruptedException {
+
+       //заходим на сайт АБ и переходим в карты
+       driver.get("https://alfabank.ru/");
+       sleep(2000);
+       WebElement findBancCard = driver.findElement(By.xpath
+               ("//*[@id=\"home_page\"]/div[1]/div[1]/div/div/div[4]/ul[1]/li[2]/a"));
+       findBancCard.click();
+       sleep(4000);
+
+       //Выбор дебетовой карты
+       WebElement clickDebetCard = driver.findElement(By.xpath
+               ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[1]/div/div[2]/ul/li[1]/a"));
+       clickDebetCard.click();
+       sleep(4000);
+
+       //Выбор МастерКард и тип Классик
+       WebElement clickMasterCard = driver.findElement(By.xpath
+               ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[2]/div[1]/div[1]/div/form/div/div[3]/div/div/div/div[1]/label"));
+       clickMasterCard.click();
+
+       WebElement clickClassic = driver.findElement(By.xpath
+               ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[2]/div[1]/div[1]/div/form/div/div[4]/div/div/div/div[1]/label"));
+       clickClassic.click();
+       sleep(4000);
+
+       WebElement checkCard = driver.findElement(By.xpath
+               ("//*[@id=\"product_page_list\"]/div[2]/div[2]/div[2]/div[2]/div/div[3]/div[6]/div/div[3]/h3/a"));
+
+       String expectedTitle = "Карта «Перекресток»";
+       assertEquals(expectedTitle, checkCard.getText()); //сравнение что ожидали и что получили.
+    }
+    @AfterClass
+    public static void closeBrowser () {
+        driver.close();
     }
 }
